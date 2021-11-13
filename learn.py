53547b1cebae1662f11b04859f0d52942333b3ca
Axshavan <axshavan@gmail.com>
2021-11-13 20:12:19 +0100

    segments

--- learn.py
+++ learn.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 import sys
-from PIL import Image
+from PIL import Image, ImageDraw
 import wg5borders
 
 if len(sys.argv) < 2:
@@ -26,12 +26,33 @@ for x in range(image.width):
 wg5borders_grid = wg5borders.grid(pixelmap)
 wg5borders_grid = wg5borders.grid_gradients(wg5borders_grid)
 wg5borders_grid = wg5borders.border_points_from_grid(pixelmap, wg5borders_grid)
-wg5borders_grid = wg5borders.reduce_border_points(wg5borders_grid)
+wg5borders_segments = wg5borders.segment_from_border_points(wg5borders_grid)
+wg5borders_segments = wg5borders.linear_reduce_segments(wg5borders_segments)
 
-print(len(wg5borders_grid))
+# dump border segments to the picture
+draw = ImageDraw.Draw(image)
+for col in wg5borders_segments:
+    for cell in col:
+        for segment in cell:
+            if segment is not False:
+                draw.line((segment.start_x, segment.start_y, segment.end_x, segment.end_y), fill=128)
+                image.putpixel((segment.start_x, segment.start_y), (255, 0, 0))
+                image.putpixel((segment.end_x, segment.end_y), (0, 0, 255))
 
-# dump borders to the picture
-for point in wg5borders_grid:
-    image.putpixel((point.x, point.y), (0, 0, 0))
 
-image.save('out.jpg')
+# dump border points to the picture
+#for col in wg5borders_grid:
+#    for cell in col:
+#        for point in cell:
+#            pixel = (0, 0, 0)
+#            if point.direction == 1:
+#                pixel = (255, 0, 0)
+#            elif point.direction == 2:
+#                pixel = (0, 255, 0)
+#            elif point.direction == 3:
+#                pixel = (0, 0, 255)
+#            elif point.direction == 4:
+#                pixel = (255, 128, 0)
+#            image.putpixel((point.x, point.y), pixel)
+
+image.save('out.jpg', quality=95)
--- wg5borders.py
+++ wg5borders.py
@@ -20,6 +20,14 @@ class WG5BorderPoint:
     direction = 0  # 0 - not sure, 1 - vertical, 2 - diagonal bl-tr, 3 - horizontal, 4 - diagonal tl-br
 
 
+class WG5Segment:
+    start_x = 0
+    start_y = 0
+    end_x = 0
+    end_y = 0
+    rotation = 0
+    direction = 0  # 0 - not sure, 1 - vertical, 2 - diagonal bl-tr, 3 - horizontal, 4 - diagonal tl-br
+
 def grayscale_from_rgb(pixel) -> float:
     return (pixel[0] + pixel[1] + pixel[2]) / 3  # todo: make it less ugly
 
@@ -28,13 +36,12 @@ def grid(pixelmap, grid_step: int = 0) -> list:
     grid_step = int(grid_step)
     if grid_step < 1:
         grid_step = GRID_STEP
-    x = 0
     grid = []
-    for col in pixelmap:
+    for x in range(0, len(pixelmap)):
         if x % grid_step == 0:
             y = 0
             grid_col = []
-            for pixel in col:
+            for pixel in pixelmap[x]:
                 if y % grid_step == 0:
                     grid_node = WG5GridNode()
                     grid_node.x = x
@@ -50,19 +57,16 @@ def grid(pixelmap, grid_step: int = 0) -> list:
 def grid_gradients(grid: list) -> list:
     max_x = len(grid) - 1
     max_y = len(grid[max_x]) - 1
-    x = 0
-    for col in grid:
-        y = 0
-        for node in col:
+    for x in range(0, max_x + 1):
+        for y in range(0, max_y + 1):
             if y > 0 and x < max_x:
-                node.gradient_tr = node.pixel - grid[x + 1][y - 1].pixel
+                grid[x][y].gradient_tr = grid[x][y].pixel - grid[x + 1][y - 1].pixel
             if y < max_y:
-                node.gradient_b = node.pixel - grid[x][y + 1].pixel
+                grid[x][y].gradient_b = grid[x][y].pixel - grid[x][y + 1].pixel
                 if x < max_x:
-                    node.gradient_br = node.pixel - grid[x + 1][y + 1].pixel
+                    grid[x][y].gradient_br = grid[x][y].pixel - grid[x + 1][y + 1].pixel
             if x < max_x:
-                node.gradient_r = node.pixel - grid[x + 1][y].pixel
-            grid[x][y] = node
+                grid[x][y].gradient_r = grid[x][y].pixel - grid[x + 1][y].pixel
             y += 1
         x += 1
     return grid
@@ -87,8 +91,10 @@ def border_points_from_grid(pixelmap: list, grid: list) -> list:
     pixelmap_maxx = len(pixelmap) - 1
     pixelmap_maxy = len(pixelmap[pixelmap_maxx]) - 1
     for col in grid:
+        border_points_col = []
+        y = 0
         for node in col:
-            y = 0
+            border_points_cell = []
             if abs(node.gradient_tr) > BORDER_THRESHOLD:
                 border_point = WG5BorderPoint()
                 border_point.direction = 4
@@ -102,7 +108,7 @@ def border_points_from_grid(pixelmap: list, grid: list) -> list:
                 i = get_gradient_middle(gradient_set)
                 border_point.x = node.x + i
                 border_point.y = node.y - i
-                border_points.append(border_point)
+                border_points_cell.append(border_point)
             if abs(node.gradient_r) > BORDER_THRESHOLD:
                 border_point = WG5BorderPoint()
                 border_point.direction = 1
@@ -114,7 +120,7 @@ def border_points_from_grid(pixelmap: list, grid: list) -> list:
                 i = get_gradient_middle(gradient_set)
                 border_point.x = node.x + i
                 border_point.y = node.y
-                border_points.append(border_point)
+                border_points_cell.append(border_point)
             if abs(node.gradient_b) > BORDER_THRESHOLD:
                 border_point = WG5BorderPoint()
                 border_point.direction = 3
@@ -126,7 +132,7 @@ def border_points_from_grid(pixelmap: list, grid: list) -> list:
                 i = get_gradient_middle(gradient_set)
                 border_point.x = node.x
                 border_point.y = node.y + i
-                border_points.append(border_point)
+                border_points_cell.append(border_point)
             if abs(node.gradient_br) > BORDER_THRESHOLD:
                 border_point = WG5BorderPoint()
                 border_point.direction = 2
@@ -140,28 +146,110 @@ def border_points_from_grid(pixelmap: list, grid: list) -> list:
                 i = get_gradient_middle(gradient_set)
                 border_point.x = node.x + i
                 border_point.y = node.y + i
-                border_points.append(border_point)
+                border_points_cell.append(border_point)
+            border_points_col.append(border_points_cell)
             y += 1
+        border_points.append(border_points_col)
         x += 1
     return border_points
 
 
-def reduce_border_points(border_points: list) -> list:
-    reduced_border_points = []
-    for i in range(0, len(border_points) - 1):
-        if border_points[i].direction == 0:
-            continue
-        for j in range(i + 1, len(border_points)):
-            if border_points[j].x - 1 <= border_points[i].x <= border_points[j].x + 1 and border_points[j].y - 1 <= border_points[i].y <= border_points[j].y + 1:
-                border_points[j].direction = 0
-            else:
-                if border_points[i].x == border_points[j].x and abs(border_points[i].y - border_points[j].y) <= GRID_STEP:
-                    for k in range(j + 1, len(border_points)):
-                        if border_points[j].x == border_points[k].x and abs(border_points[j].y - border_points[k].y) <= GRID_STEP:
-                            border_points[j].direction = 0
-                if border_points[i].y == border_points[j].y and abs(border_points[i].x - border_points[j].x) <= GRID_STEP:
-                    for k in range(j + 1, len(border_points)):
-                        if border_points[j].y == border_points[k].y and abs(border_points[j].x - border_points[k].x) <= GRID_STEP:
-                            border_points[j].direction = 0
-        reduced_border_points.append(border_points[i])
-    return reduced_border_points
+def segment_from_border_points(grid: list) -> list:
+    segments_list = []
+    grid_maxx = len(grid) - 1
+    grid_maxy = len(grid[grid_maxx]) - 1
+    for x in range(0, grid_maxx + 1):
+        segments_list_col = []
+        for y in range(0, grid_maxy + 1):
+            segments_list_cell = []
+            for c in grid[x][y]:
+                if (c.direction == 0 or c.direction == 1) and y < grid_maxy:
+                    for r in grid[x][y + 1]:
+                        if r.direction == 0 or r.direction == 1:
+                            segment = WG5Segment()
+                            segment.start_x = c.x
+                            segment.start_y = c.y
+                            segment.end_x = r.x
+                            segment.end_y = r.y
+                            segment.rotation = 0
+                            segment.direction = 1
+                            segments_list_cell.append(segment)
+                            break
+                if (c.direction == 0 or c.direction == 2) and 0 < y and x < grid_maxx:
+                    for r in grid[x + 1][y - 1]:
+                        if r.direction == 0 or r.direction == 2:
+                            segment = WG5Segment()
+                            segment.start_x = c.x
+                            segment.start_y = c.y
+                            segment.end_x = r.x
+                            segment.end_y = r.y
+                            segment.rotation = 90
+                            segment.direction = 2
+                            segments_list_cell.append(segment)
+                            break
+                if (c.direction == 0 or c.direction == 3) and x < grid_maxx:
+                    for r in grid[x + 1][y]:
+                        if r.direction == 0 or r.direction == 3:
+                            segment = WG5Segment()
+                            segment.start_x = c.x
+                            segment.start_y = c.y
+                            segment.end_x = r.x
+                            segment.end_y = r.y
+                            segment.rotation = 45
+                            segment.direction = 3
+                            segments_list_cell.append(segment)
+                            break
+                if (c.direction == 0 or c.direction == 4) and y < grid_maxy and x < grid_maxx:
+                    for r in grid[x + 1][y + 1]:
+                        if r.direction == 0 or r.direction == 4:
+                            segment = WG5Segment()
+                            segment.start_x = c.x
+                            segment.start_y = c.y
+                            segment.end_x = r.x
+                            segment.end_y = r.y
+                            segment.rotation = -45
+                            segment.direction = 4
+                            segments_list_cell.append(segment)
+                            break
+            segments_list_col.append(segments_list_cell)
+        segments_list.append(segments_list_col)
+    return segments_list
+
+
+def linear_reduce_segments(segments_list: list) -> list:
+    maxx = len(segments_list) - 1
+    maxy = len(segments_list[maxx]) - 1
+    for x in range(0, maxx + 1):
+        for y in range(0, maxy + 1):
+            for i in range(0, len(segments_list[x][y])):
+                if segments_list[x][y][i] is not False:
+                    if (segments_list[x][y][i].direction == 0 or segments_list[x][y][i].direction == 1) and y < maxy:
+                        for j in range(0, len(segments_list[x][y + 1])):
+                            if segments_list[x][y + 1][j] is not False and (segments_list[x][y + 1][j].direction == 0 or segments_list[x][y + 1][j].direction == 1):
+                                segments_list[x][y][i].end_x = segments_list[x][y + 1][j].end_x
+                                segments_list[x][y][i].end_y = segments_list[x][y + 1][j].end_y
+                                segments_list[x][y + 1][j] = False
+                                break
+                    if (segments_list[x][y][i].direction == 0 or segments_list[x][y][i].direction == 2) and 0 < y and x < maxx:
+                        for j in range(0, len(segments_list[x + 1][y - 1])):
+                            if segments_list[x + 1][y - 1][j] is not False and (segments_list[x + 1][y - 1][j].direction == 0 or segments_list[x + 1][y - 1][j].direction == 2):
+                                segments_list[x][y][i].end_x = segments_list[x + 1][y - 1][j].end_x
+                                segments_list[x][y][i].end_y = segments_list[x + 1][y - 1][j].end_y
+                                segments_list[x + 1][y - 1][j] = False
+                                break
+                    if (segments_list[x][y][i].direction == 0 or segments_list[x][y][i].direction == 3) and x < maxx:
+                        for j in range(0, len(segments_list[x + 1][y])):
+                            if segments_list[x + 1][y][j] is not False and (segments_list[x + 1][y][j].direction == 0 or segments_list[x + 1][y][j].direction == 3):
+                                segments_list[x][y][i].end_x = segments_list[x + 1][y][j].end_x
+                                segments_list[x][y][i].end_y = segments_list[x + 1][y][j].end_y
+                                segments_list[x + 1][y][j] = False
+                                break
+                    if (segments_list[x][y][i].direction == 0 or segments_list[x][y][i].direction == 4) and y < maxy and x < maxx:
+                        for j in range(0, len(segments_list[x + 1][y + 1])):
+                            if segments_list[x + 1][y + 1][j] is not False and (segments_list[x + 1][y + 1][j].direction == 0 or segments_list[x + 1][y + 1][j].direction == 4):
+                                segments_list[x][y][i].end_x = segments_list[x + 1][y + 1][j].end_x
+                                segments_list[x][y][i].end_y = segments_list[x + 1][y + 1][j].end_y
+                                segments_list[x + 1][y + 1][j] = False
+                                break
+
+    return segments_list
